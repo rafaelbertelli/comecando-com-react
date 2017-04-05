c0dd1a6a82dfcc30ed2463c4ff5bb78db82b6625
Rafael Bertelli <rafaelbertelli89@gmail.com>
2017-04-04 23:37:36 -0300

    Projeto dividido em ES5 ES6

---
+++ React ES6/app/App.js
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+var GitHub = require('./components/GitHub.js');
+
+ReactDom.render(<GitHub />, document.getElementById('app'));
---
+++ React ES6/webpack.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  entry: ["./app/App.js"],
+  output: {
+    filename: "public/bundle.js"
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true, 
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
